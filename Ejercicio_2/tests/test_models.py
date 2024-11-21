a3c68db4fd316c9064e610de634b051b93b9bc83
Adrian <mgallo215@gmail.com>
2024-11-21 10:17:06 -0500

    test_models

--- Ejercicio_2/tests/test_models.py
+++ Ejercicio_2/tests/test_models.py
@@ -1,1 +1,27 @@
-from unittest import mock
+import unittest
+import pytest
+from unittest.mock import Mock, MagicMock
+from repository import InMemoryPostRepository, InMemoryUserRepository
+from models import User, Post
+
+class TestRepository(unittest.TestCase):
+
+    def test_add_user(self):
+        fake_repo = InMemoryUserRepository()
+        user = User('test_user', 'test_password01')
+        fake_repo.addUser(user)
+
+        assert fake_repo.userList == {user.getId(), user}
+
+    def test_update_user(self):
+        fake_repo = InMemoryUserRepository()
+        user = User('test_user', 'test_password01')
+        fake_repo.addUser(user)
+        user.username = 'test_user_2'
+
+        fake_repo.updateUser(user)
+
+        assert fake_repo.userList[user.getId()].username == 'test_user_2'
+
+    def test_delete_user_valid(self):
+         
