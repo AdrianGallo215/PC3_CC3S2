f7118f79c95221ed9d5044ebba2f80e02d64a60c
Adrian <mgallo215@gmail.com>
2024-11-24 23:40:38 -0500

    utils.py añadido

---
+++ Ejercicio_2/utils.py
@@ -0,0 +1,18 @@
+import re
+import logging
+
+def is_valid_email(email):
+    regex = r'^\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    return re.match(regex, email) is not None
+
+
+def setup_logger(name, log_file, level=logging.INFO):
+    handler = logging.FileHandler(log_file)
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+
+    return logger
