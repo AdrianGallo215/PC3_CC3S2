248cdd09fa412f0a6f3b71dfa6eb6b2a2fc3307c
Adrian <mgallo215@gmail.com>
2024-12-01 20:54:01 -0500

    add document interface

--- Ejercicio 4/src/models/document.py
+++ Ejercicio 4/src/models/document.py
@@ -1,4 +1,20 @@
-class Document():
+from abc import ABC, abstractmethod
+
+class DocumentInterface(ABC):
+
+    @abstractmethod
+    def get_id(self):
+        pass
+
+    @abstractmethod
+    def get_content(self):
+        pass
+
+    @abstractmethod
+    def __str__(self):
+        pass
+
+class TextDocument(DocumentInterface):
 
     documentCount = 0
 
@@ -14,8 +30,8 @@ class Document():
     def __str__(self):
         return(f"Documento {self.id}: {self.contenido[:30]}...")
 
-    def read(self):
-        print(self.contenido)
+    def get_content(self):
+        return self.contenido
     
     def getId(self):
         return self.id
